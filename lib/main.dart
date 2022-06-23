2bbb28f1ee25bf2f879de96bc4bd3458a98ba3ab
LucasTecDev <lucasplussoft@gmail.com>
2022-06-23 16:09:48 -0300

    WebView_Flutter

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          toolbarHeight: 2,
+        ),
+        body: WebView(initialUrl: "https://novo.plussoft.com.br/",
+        javascriptMode: JavascriptMode.unrestricted,
+        )
+        
+      ),
+    );
+  }
+}
